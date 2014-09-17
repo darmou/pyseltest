2735772ab0c03b404e94889e3e21beef267491c2
Andrew Magee <amagee@gmail.com>
2014-09-17 21:12:32 +1000

    initial

---
+++ conftest.py
@@ -0,0 +1,98 @@
+import os
+import pytest
+from tests.helpers import MyTestDriver
+from app import create_app
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser",
+        action="store",
+        default="firefox"
+    )
+
+
+
+@pytest.yield_fixture(scope='session')
+def app():
+    a = create_app("config.TestingConfig")
+    with a.app_context():
+        a.drop_all()
+        a.init_db()
+        a.connection = a.engine.connect()
+        a.db_session.configure(bind=a.connection)
+        yield a
+        a.connection.close()
+
+
+@pytest.yield_fixture(scope="function")
+def session(app):
+    """
+    Creates a new database session (with working transaction)
+    for a test duration.
+    """
+    app.transaction = app.connection.begin()
+
+    # pushing new Flask application context for multiple-thread
+    # tests to work
+    ctx = app.app_context()
+    ctx.push()
+
+    session = app.db_session()
+
+    yield session
+
+    # the code after yield statement works as a teardown
+    app.transaction.close()
+    session.close()
+    ctx.pop()
+
+
+@pytest.yield_fixture
+def xvfb(request):
+    from xvfbwrapper import Xvfb
+
+    vdisplay = Xvfb()
+    vdisplay.start()
+
+    yield
+
+    vdisplay.stop()
+
+
+@pytest.yield_fixture
+def browser(request):
+    """
+    http://chromedriver.storage.googleapis.com/index.html
+    """
+
+    from selenium import webdriver
+    browser_name = request.config.getoption("--browser")
+    if browser_name == "firefox":
+        browser = webdriver.Firefox()
+    elif browser_name == "phantomjs":
+        browser = webdriver.PhantomJS()
+    elif browser_name == "chrome":
+        browser = webdriver.Chrome("./chromedriver")
+    else:
+        raise Exception("Invalid browser name")
+    yield browser
+    browser.quit()
+
+
+@pytest.yield_fixture
+def driver(browser):
+    import config
+    yield MyTestDriver(
+        browser=browser, 
+        base_url="http://" + config.TestingConfig.SERVER_NAME
+    )
+
+
+@pytest.yield_fixture
+def xvfb_browser(xvfb, browser):
+    yield browser
+
+
+@pytest.yield_fixture
+def test_client(app):
+    yield app.test_client()
---
+++ helpers.py
@@ -0,0 +1,39 @@
+class MyTestDriver:
+    def __init__(self, browser, base_url):
+        self.browser = browser
+        self.base_url = base_url
+
+    def open_page(self, path):
+        return self.browser.get(self.base_url + path)
+
+    def get_element(self, css=None, model=None):
+        list = self.get_elements(css=css, model=model)
+        if list == []:
+            raise Exception("Not found")
+        if len(list) > 1:
+            raise Exception("Too many")
+        return list[0]
+
+    def get_elements(self, css=None, model=None, text=None):
+        if css is not None:
+            return self.browser.find_elements_by_css_selector(css)
+        elif model is not None:
+            return self.get_elements(css="[ng-model='{}']".format(model))
+        elif text is not None:
+            return [e for e in self.get_elements(css="*") if e.text == text]
+        else:
+            raise ValueError()
+
+    def element_exists(self, **kwargs):
+        return self.get_elements(**kwargs) != []
+
+    def wait_until(self, func, timeout=2):
+        WebDriverWait(self.browser, timeout).until(func)
+
+    def wait_for_element(self, css=None, model=None, timeout=2):
+        self.wait_until(lambda br: br.get_elements(css=css, model=model) != [], timeout)
+
+
+
+
+
